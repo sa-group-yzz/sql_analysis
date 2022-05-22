e879d7c75744202043a0e5b57e01bb890c9f304d
zzc <2272713550@qq.com>
2022-05-22 13:34:58 +0800

    +e

--- run_test.sh
+++ run_test.sh
@@ -7,6 +7,7 @@ echo "compile test cases..."
 mvn test-compile
 END=24
 echo "run tests..."
+set +e
 for ((i=1;i<=END;i++)); do
     java -jar ./target/analysis-1.0-SNAPSHOT-jar-with-dependencies.jar -a assertions -t target/test-classes -c Case$i
 done
